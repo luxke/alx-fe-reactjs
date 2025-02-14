80cdf4e0de163ecaac8e47257aeb9c3304cb4dda
luxke <kipkemboifelix576@gmail.com>
2025-02-15 02:26:57 +0300

    made more changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,13 +3,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+function App() {
+  return (
+      <div>
+          <WelcomeMessage />
+      </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div><WelcomeMessage /></div>
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
