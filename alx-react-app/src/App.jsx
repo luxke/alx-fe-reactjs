dcd2ee49e4d02f2ddfbb440bd807cc9ef8892d88
luxke <kipkemboifelix576@gmail.com>
2025-02-18 06:29:52 +0300

    added src/components/welcomemessage.jsx and modified App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,23 +1,15 @@
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from "./components/WelcomeMessage"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  return (
-      <div>
-          <WelcomeMessage />
-      </div>
-  );
-}
-
-function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
