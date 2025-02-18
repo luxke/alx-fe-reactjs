93c897c354e6d09de63b5b5347d37d41f960986f
luxke <kipkemboifelix576@gmail.com>
2025-02-18 07:20:09 +0300

    i have created three other components component/Header component/MainContent component/Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
-import WelcomeMessage from "./components/WelcomeMessage"
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +12,9 @@ function App() {
 
   return (
     <>
+      <Header />
+      <MainContent />
+      <Footer />
       <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
