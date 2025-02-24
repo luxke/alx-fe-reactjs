6465342333788b4b0563aa5ab65c5625f63018d4
luxke <kipkemboifelix576@gmail.com>
2025-02-24 22:14:59 +0300

    added counter.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Counter from './src/components'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -19,6 +19,7 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <Counter />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
