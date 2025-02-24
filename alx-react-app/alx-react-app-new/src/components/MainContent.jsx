88290aaf48a10f995e56bc1ea7380b05c25145a3
luxke <kipkemboifelix576@gmail.com>
2025-02-24 22:35:21 +0300

    added a duplicate folder

---
+++ alx-react-app/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent () {
+ return (
+<main style={{ padding: '20px', textAlign: 'center', fontSize: '18px', backgroundColor: '#e3f2fd' }} >
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+ );
+}
+export default MainContent;
