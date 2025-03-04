218e753536e823b8dbaa5d42ef6c7dbcfe024890
luxke <kipkemboifelix576@gmail.com>
2025-03-04 23:52:39 +0300

    created a new file

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,11 +8,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './src/components'
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <>
+<UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+
       <h1>UserProfile</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       <Header />
