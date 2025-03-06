33e2eb8e267bd86c0ffdb3f23b1650e31f3ef55f
luxke <kipkemboifelix576@gmail.com>
2025-03-06 23:40:20 +0300

    made more changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -13,6 +21,16 @@ function App() {
 
   return (
     <>
+       <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
       <h1>UserProfile</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
       <Header />
