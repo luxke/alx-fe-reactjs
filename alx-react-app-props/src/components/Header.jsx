3443480cc255bd54e612fa31987b115bf5c66833
luxke <kipkemboifelix576@gmail.com>
2025-03-15 21:46:53 +0300

    made changes on header.jsx

--- alx-react-app-props/src/components/Header.jsx
+++ alx-react-app-props/src/components/Header.jsx
@@ -5,6 +5,7 @@ function Header () {
   </header>
   );
 }
+
  export default Header;
 <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
    <h1>My Favorite Cities</h1>
