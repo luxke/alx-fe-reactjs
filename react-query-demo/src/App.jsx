d5dce06637416ce302e8040a1856749aa449ff68
luxke <kipkemboifelix576@gmail.com>
2025-03-17 21:23:21 +0300

    made different changes on App.js and Postscomponent

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PostsComponent from './components/PostsComponent';
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={QueryClient}>
       <PostsComponent />
     </QueryClientProvider>
 
