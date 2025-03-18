f6106bf14dffd4bda1550edb9b3c64660bc794d5
luxke <kipkemboifelix576@gmail.com>
2025-03-18 22:53:43 +0300

    updated postcomponent

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+
+function fetchPosts() {
+  return fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    });
+}
+
+function PostsComponent() {
+  const { data, error, isLoading, isError, refetch } = useQuery('posts', fetchPosts, {
+    staleTime: 5000, 
+    cacheTime: 10000, 
+    refetchOnWindowFocus: false, 
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default PostsComponent;
