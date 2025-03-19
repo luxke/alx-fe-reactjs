bbf3c0b12cea10e1d9cd25c446953b9345760806
luxke <kipkemboifelix576@gmail.com>
2025-03-19 13:37:18 +0300

    made changes on App.jsx

--- react-todo/src/components/App.jsx
+++ react-todo/src/components/App.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import TodoList from "./components/TodoList";
+import TodoList from "./TodoList";
 
 function App() {
   return (
