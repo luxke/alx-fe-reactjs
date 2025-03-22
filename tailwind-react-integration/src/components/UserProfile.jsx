5615279876c9c727b3f10ccdc2aee8a73a85a2e0
luxke <kipkemboifelix576@gmail.com>
2025-03-22 15:31:10 +0300

    made changes on userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,13 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
         src="https://images.app.goo.gl/tUBasoVGw3SoywUP9" 
         alt="User" 
-        className="rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out" 
       />
-      <h1 className="text-blue-800 sm:text-lg md:text-xl my-4">John Doe</h1>
-      <p className="text-gray-600 sm:text-sm md:text-base">
-        Developer at Example Co. Loves to write code and explore new technologies.
-      </p>
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300 ease-in-out">John Doe</h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
