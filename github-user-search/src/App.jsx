7c6670d585c5a06dd0218e798ee1fe0efeb613bf
luxke <kipkemboifelix576@gmail.com>
2025-04-03 11:40:00 +0300

    made changes by updating APP.JSX

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+/*import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css'*/
+import SearchBar from "./components/SearchBar";
+import UserCard from "./components/UserCard";
+import ErrorMessage from "./components/ErrorMessage";
+import Loader from "./components/Loader";
+import { fetchGitHubUser } from "./services/githubService";
 
 function App() {
   const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
+  const handleSearch = async (username) => {
+    setLoading(true);
+    setError("");
+    setUser(null);
+    
+    const userData = await fetchGitHubUser(username);
+    setLoading(false);
+
+    if (!userData) {
+      setError("User not found!");
+    } else {
+      setUser(userData);
+    }
+  };
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-lg mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-4">GitHub User Search</h1>
+      <SearchBar onSearch={handleSearch} />
+      {loading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+      {user && <UserCard user={user} />}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
